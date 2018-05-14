876c018402950c71ebf1b1cb8ca336c2c137df07
liangbc <237702131@qq.com>
2018-05-14 11:24:29 +0800

    修改版本

--- src/main/java/com/love/baby/monitor/MonitorApplication.java
+++ src/main/java/com/love/baby/monitor/MonitorApplication.java
@@ -4,18 +4,16 @@ package com.love.baby.monitor;
  * Created by liangbc on 2017/8/26.
  */
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by liangbc on 2017/7/25.
  */
-@EnableDiscoveryClient
+@Configuration
 @EnableAutoConfiguration
-@SpringBootApplication
 @EnableAdminServer
 public class MonitorApplication {
     public static void main(String[] args) {
