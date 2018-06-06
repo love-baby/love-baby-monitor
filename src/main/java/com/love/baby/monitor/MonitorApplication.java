fb6a9d6da5db7ed8908289ee25a7c8a41ce8e805
liangbc <bicong.liang@51zouchuqu.com>
2018-06-06 20:55:31 +0800

    配置文件

--- src/main/java/com/love/baby/monitor/MonitorApplication.java
+++ src/main/java/com/love/baby/monitor/MonitorApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by liangbc on 2017/7/25.
@@ -22,14 +20,5 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class MonitorApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonitorApplication.class, args);
-    }
-
-    @Configuration
-    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().permitAll()
-                    .and().csrf().disable();
-        }
     }
 }
