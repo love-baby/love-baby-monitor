a2721c3f9336230f259edaa1914aae9c672e056d
liangbc <bicong.liang@51zouchuqu.com>
2018-06-07 11:13:53 +0800

    配置文件

--- src/main/java/com/love/baby/monitor/MonitorApplication.java
+++ src/main/java/com/love/baby/monitor/MonitorApplication.java
@@ -4,14 +4,14 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 23770
  */
 @EnableAutoConfiguration
 @EnableAdminServer
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class MonitorApplication {
     public static void main(String[] args) {
