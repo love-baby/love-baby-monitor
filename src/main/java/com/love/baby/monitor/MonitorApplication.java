9c707d6d696f2679fadb53c3c5be0b648ba39a25
liangbc <bicong.liang@51zouchuqu.com>
2018-06-07 13:54:40 +0800

    配置文件

--- src/main/java/com/love/baby/monitor/MonitorApplication.java
+++ src/main/java/com/love/baby/monitor/MonitorApplication.java
@@ -3,7 +3,6 @@ package com.love.baby.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableAdminServer
-@SpringBootApplication
 public class MonitorApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonitorApplication.class, args);
